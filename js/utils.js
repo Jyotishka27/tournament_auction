8e62b68f36c7af45000d4272c901f1cef2934005
Jyotishka Mukhopadhyay <76933417+Jyotishka27@users.noreply.github.com>
2026-01-20 09:44:06 +0530

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,15 @@
+function fmt(num) {
+  return new Intl.NumberFormat('en-IN').format(num);
+}
 
+function catLabel(cat) {
+  return CATEGORY_LABELS[cat] || 'Unknown';
+}
+
+function applyUnsoldReduction(player) {
+  player.unsoldCount = (player.unsoldCount || 0) + 1;
+  player.basePrice = Math.max(
+    Math.floor(player.basePrice * REDUCTION_FACTOR),
+    MIN_PRICE
+  );
+}
