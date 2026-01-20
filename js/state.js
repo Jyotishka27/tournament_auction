056df6b174092d28a37f6e44ccac2ff1d3f557a5
Jyotishka Mukhopadhyay <76933417+Jyotishka27@users.noreply.github.com>
2026-01-20 09:43:44 +0530

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,1 +1,9 @@
-
+const state = {
+  pools: { X: [], P: [], A: [], B: [], C: [], UNSOLD: [] },
+  skipped: { X: [], P: [], A: [], B: [], C: [], UNSOLD: [] },
+  category: 'X',
+  current: null,
+  teams: [],
+  sales: [],
+  timer: { handle: null, left: 0, running: false }
+};
